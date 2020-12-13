9a487f1cadb095fdfad49bb01ba63e3f1282e0e2
Yevgeniy <anigeka@gmail.com>
2020-12-13 12:59:59 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        actor = Actor.find_by(first_name: self.first_name, last_name: self.last_name)
+        "#{actor.first_name} #{actor.last_name}"
+    end
+
+    def list_roles
+        character = Character.find_by(actor_id: self.id)
+        "#{character.name} - #{Show.find(character.show_id).name}"
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def network       
+        Network.find(self.network_id) if self.network_id
+    end
+
+    def build_network(call_letters:)
+        n = Network.find_or_create_by(call_letters: call_letters)
+        self.network_id = n.id
+        self.save
+        n
+    end
+
+    def actors_list
+        self.actors.map { |a| "#{a.first_name} #{a.last_name}" }
+    end
 end
